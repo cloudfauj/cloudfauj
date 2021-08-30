da98d019174707c4d806dc3a9016325149b9f434
duaraghav8 <12758282+duaraghav8@users.noreply.github.com>
2021-08-30 23:58:38 +0530

    add api client code for domain commands

---
+++ api/domain.go
@@ -0,0 +1,11 @@
+package api
+
+import "github.com/cloudfauj/cloudfauj/server"
+
+func (a *API) AddDomain(name string) (<-chan *server.Event, error) {
+	return a.makeWebsocketRequest(a.constructWsURL("/domain/"+name+"/add"), nil)
+}
+
+func (a *API) DeleteDomain(name string) (<-chan *server.Event, error) {
+	return a.makeWebsocketRequest(a.constructWsURL("/domain/"+name+"/add"), nil)
+}
